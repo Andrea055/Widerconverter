566b5779e44af1630faeff7c266e1f7b9087ab16
Andrea055 <vivanco11.ac@gmail.com>
2021-04-30 11:11:04 +0200

    Add pdf to txt
    use fpdf library

--- src/HotFolders.py
+++ src/HotFolders.py
@@ -22,13 +22,15 @@ import uuid
 import requests
 import shutil
 from requests import Response
+from fpdf import FPDF
 
 
-client = commands.Bot(command_prefix="+")  
+client = commands.Bot(command_prefix=".")  
 config_file = open('hotfolders_config.json', 'r')
 config_info = json.load(config_file)
 api_key = config_info['api_key'][0]
 config_info = config_info["conversions"]
+pdf = FPDF()   
 
 
 # This will hold the watch objects for the paths
@@ -50,7 +52,7 @@ async def on_ready():
     print("Bot is ready")
 
 @client.command()                                       # wide command and processing 
-async def convert(ctx):
+async def converttxt(ctx):
 
     try:
         url = ctx.message.attachments[0].url            # check for an image, call exception if none found
@@ -60,16 +62,41 @@ async def convert(ctx):
     else:
         if url[0:26] == "https://cdn.discordapp.com":   # look to see if url is from discord
             r = requests.get(url, stream=True)
-            imageName = 'fullsized_image' + '.png'      # save image to wide
-            with open(imageName, 'wb') as out_file:
-                print('Saving image: ' + imageName)
-                shutil.copyfileobj(r.raw, out_file)
-                time.sleep(15)                          # Sleep for 15 seconds 
-                await ctx.send('In .gif', file=discord.File('fullsized_image.gif')) # send image in server discord
-                time.sleep(5)                          # Sleep for 5 seconds 
-                os.remove("fullsized_image.gif")       # Destroy file to no-loop program
-                
+            open('url', 'wb').write(r.content)
+            os.rename(r'E:\BOT\bot-env\zamzar-samples-hotfolders-master\src\url',r'E:\BOT\bot-env\zamzar-samples-hotfolders-master\src\url.txt')
+            pdf.add_page()
+            pdf.set_font("Arial", size = 15)
+            f = open("url.txt", "r")
+            for x in f:
+                pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+                pdf.output("url.pdf")  
+            await ctx.send('I have a PDF for you!' , file=discord.File('url.pdf'))
+            time.sleep(5)
+            os.remove("url.pdf")
+            os.remove("url.txt")
 
+@client.command()                                       # wide command and processing 
+async def imagetogif(ctx):
+    try:
+        url = ctx.message.attachments[0].url            # check for an image, call exception if none found
+    except IndexError:
+        print("Error: No attachments")
+        await ctx.send("No attachments detected!")
+    else:
+        if url[0:26] == "https://cdn.discordapp.com":   # look to see if url is from discord
+            r = requests.get(url, stream=True)
+            open('url', 'wb').write(r.content)
+            os.rename(r'E:\BOT\bot-env\zamzar-samples-hotfolders-master\src\url',r'E:\BOT\bot-env\zamzar-samples-hotfolders-master\src\url.png')
+            time.sleep(25)
+            await ctx.send('Gif', file=discord.File('url.gif'))
+            time.sleep(3)
+            os.remove('url.gif')
+                
+@client.command()
+async def test(ctx):
+    attachment_url = ctx.message.attachments[0].url
+    file_request = requests.get(attachment_url)
+    attachment.save()
 
 
 
